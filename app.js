d6ce1d928652d2ee557910a6cf8e4a598e7e9d0d
yongjin92 <rnjsdydwls09@naver.com>
2016-12-10 20:59:19 +0900

    four

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ var session = require('express-session');
 var methodOverride = require('method-override');
 var flash = require('connect-flash');
 var mongoose   = require('mongoose');
+var passport = require('passport');
+var configAuth = require('./config/auth');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var routeAuth = require('./routes/auth');
+
 
 var app = express();
 
@@ -40,9 +45,13 @@ app.use(session({
   secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
 }));
 app.use(flash());
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(function(req, res, next) {
   res.locals.currentUser = req.session.user;
@@ -50,8 +59,11 @@ app.use(function(req, res, next) {
   next();
 });
 
+configAuth(passport);
+
 app.use('/', routes);
 app.use('/users', users);
+routeAuth(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
